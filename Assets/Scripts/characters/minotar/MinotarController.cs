3a8e63d1376c809f5813a79673598f691b09b9cd
Zim <zim.github@gmail.com>
2015-08-04 23:17:39 +0200

    refonte IA du minotar

--- Assets/Scripts/characters/minotar/MinotarController.cs
+++ Assets/Scripts/characters/minotar/MinotarController.cs
@@ -5,9 +5,14 @@ using System.Collections;
 
 public class MinotarController : MonoBehaviour {
 
+	public float walk_speed;
+	public float run_speed;
+
+	public bool sees_player = false;
+	public bool should_update_speed = true;
+
 	private MovementScript move;
 	private PathfindingManager pathfinder;
-
 	private bool should_update_direction = true;
 
 	void Awake() {
@@ -20,14 +25,37 @@ public class MinotarController : MonoBehaviour {
 		if(should_update_direction) {
 			update_direction();
 		}
+		if (should_update_speed) {
+			update_speed();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "DetectionZone") {
+			sees_player = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.tag == "DetectionZone") {
+			sees_player = false;
+		}
 	}
 	
+	private void update_speed() {
+		if (sees_player) {
+			move.max_velocity = run_speed;
+		} else {
+			move.max_velocity = walk_speed;
+		}
+	}
+
+	private void update_direction() {
+		go_towards_point(pathfinder.get_smooth_next_move(transform.position));
+	}
+
 	private void go_towards_point(Vector2 point) {
 		Vector2 minus = (point - (Vector2)transform.position);
 		move.direction = minus;
-	}
-	
-	private void update_direction() {
-		go_towards_point(pathfinder.get_smooth_next_move(transform.position));
 	}
 }
