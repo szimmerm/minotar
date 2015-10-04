b3a0bfa0804804a862e567d529dbe8d6f745025e
Zim <zim.github@gmail.com>
2015-10-04 15:00:52 +0200

    rajout du vin

--- Assets/Scripts/characters/hero/PouletEaterScript.cs
+++ Assets/Scripts/characters/hero/PouletEaterScript.cs
@@ -2,18 +2,36 @@
 using System.Collections;
 
 public class PouletEaterScript : MonoBehaviour {
+	public int poulet_health;
+	public float poulet_score;
+	public int vin_health;
+	public float vin_score;
 
 	private HealthScript player_health;
+	private HighScoreScript score;
 
 	public void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Poulet") {
-			player_health.add_health(1);
-			Destroy(other.transform.root.gameObject);
+		if (other.transform.root.tag == "Item") {
+			string fullname = other.transform.root.name;
+			char[] delimiters = {'('};
+			switch (fullname.Split(delimiters)[0]) {
+				case "Poulet":
+					player_health.add_health(poulet_health);
+					score.add_score(poulet_score);
+					Destroy(other.transform.root.gameObject);
+					break;
+				case "Vin":
+					player_health.add_health (vin_health);
+					score.add_score (vin_score);
+					Destroy(other.transform.root.gameObject);
+					break;
+			}
 		}
 	}
 
 	// Use this for initialization
 	void Start () {
 		player_health = GetComponentInChildren<HealthScript>();
+		score = GameObject.FindGameObjectWithTag("GameController").GetComponent<HighScoreScript>();
 	}
 }
